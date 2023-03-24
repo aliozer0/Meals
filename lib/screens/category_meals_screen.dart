1c7d144cf61b8f74d548cdfad050e43001c192c1
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-03-24 22:23:49 +0300

    update

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
-import './dummy_data.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
