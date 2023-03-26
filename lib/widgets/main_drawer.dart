e1e8ca7ed378a9eab5f1e93f6dadaeded1f4ee3f
aliozer00 <92879108+aliozer00@users.noreply.github.com>
2023-03-26 14:26:26 +0300

    filters_screen added

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import '../screens/filter_screen.dart';
 
 class MainDrawer extends StatelessWidget {
   const MainDrawer({super.key});
 
-  Widget buildListTile(String title, IconData icon) {
+  Widget buildListTile(String title, IconData icon, VoidCallback tabHandler) {
     return ListTile(
       leading: Icon(
         icon,
@@ -16,7 +17,7 @@ class MainDrawer extends StatelessWidget {
             fontSize: 20,
             fontWeight: FontWeight.bold),
       ),
-      onTap: () {},
+      onTap: tabHandler,
     );
   }
 
@@ -42,8 +43,20 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          buildListTile('Meals', Icons.restaurant),
-          buildListTile('Filters', Icons.settings)
+          buildListTile(
+            'Meals',
+            Icons.restaurant,
+            () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          buildListTile(
+            'Filters',
+            Icons.settings,
+            () {
+              Navigator.of(context).pushReplacementNamed('./filters');
+            },
+          )
         ],
       ),
     );
